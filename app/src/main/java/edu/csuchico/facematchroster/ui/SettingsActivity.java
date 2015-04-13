d6ba5d5243b57a689a2e170ff93356f3b77760ab
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-04-13 12:05:23 -0700

    Add basic settings activity using PreferenceFragment

--- app/src/main/java/edu/csuchico/facematchroster/ui/SettingsActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/ui/SettingsActivity.java
@@ -1,40 +1,44 @@
 package edu.csuchico.facematchroster.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceFragment;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import edu.csuchico.facematchroster.R;
 
-public class SettingsActivity extends ActionBarActivity {
+
+public class SettingsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
 
+        Toolbar toolbar = getActionBarToolbar();
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_left);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new SettingsFragment())
+                    .commit();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Add 'general' preferences.
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
