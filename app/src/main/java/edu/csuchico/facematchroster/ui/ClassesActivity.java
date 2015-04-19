e561a5e968a69618ea6296c499c4e9023425c127
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-04-18 21:20:26 -0700

    Add a debug button in classes menu activity

--- app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
@@ -29,14 +29,12 @@ public class ClassesActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+            case R.id.debug:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
