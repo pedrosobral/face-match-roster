d47462280a14770a1cd0eac9895aaceec3114fa4
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-04-26 00:56:26 -0700

    Added RecyclerView to show list of Decks in the main Actity = ClassesActivity

--- app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
@@ -1,15 +1,24 @@
 package edu.csuchico.facematchroster.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 import edu.csuchico.facematchroster.R;
 import edu.csuchico.facematchroster.StudentLogin;
 import edu.csuchico.facematchroster.anim.ActivityTransitionAnimation;
+import edu.csuchico.facematchroster.model.Deck;
 
 import static edu.csuchico.facematchroster.util.LogUtils.makeLogTag;
 
@@ -61,5 +70,48 @@ public class ClassesActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHolder> {
+
+        private List<Deck> mDataModel = Collections.emptyList();
+
+        private LayoutInflater mInflater;
+
+        // ViewHolder class to save inflated views for recycling
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView mTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                mTextView = (TextView) findViewById(R.id.deckName);
+            }
+        }
+
+        public DeckAdapter(Context context, List<Deck> data) {
+            mInflater = LayoutInflater.from(context);
+            mDataModel = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.deck_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Deck model = mDataModel.get(position);
+
+            // TODO: Bind elements from Deck
+            holder.mTextView.setText(model.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataModel.size();
+        }
     }
 }
