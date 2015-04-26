c187b7601ec7e0aaa4e2d0b415d08bd7bedbbab2
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-04-26 15:59:08 -0700

    Added support to recyclerView, setup in onCreate and added in layout

--- app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/ui/ClassesActivity.java
@@ -1,8 +1,8 @@
 package edu.csuchico.facematchroster.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,18 +26,29 @@ import static edu.csuchico.facematchroster.util.LogUtils.makeLogTag;
 public class ClassesActivity extends BaseActivity {
     private static final String TAG = makeLogTag(ClassesActivity.class);
 
+    private RecyclerView mRecyclerView;
+    private DeckAdapter mDeckAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
 
-        findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ClassesActivity.this, FlashcardActivity.class));
-                ActivityTransitionAnimation.slide(ClassesActivity.this, ActivityTransitionAnimation.LEFT);
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mDeckAdapter = new DeckAdapter(getData());
+
+        mRecyclerView.setAdapter(mDeckAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(ClassesActivity.this));
+
+    }
+
+    private List<Deck> getData() {
+        List<Deck> listDeck = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            listDeck.add(new Deck(i, "Class: " + i, null, null, null));
+        }
+        return listDeck;
     }
 
 
@@ -72,11 +84,31 @@ public class ClassesActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHolder> {
+    public static class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHolder> {
 
         private List<Deck> mDataModel = Collections.emptyList();
 
-        private LayoutInflater mInflater;
+        public DeckAdapter(List<Deck> data) {
+            mDataModel = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.deck_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Deck deck = mDataModel.get(position);
+
+            holder.mTextView.setText(deck.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataModel.size();
+        }
 
         // ViewHolder class to save inflated views for recycling
         class ViewHolder extends RecyclerView.ViewHolder {
@@ -86,32 +118,8 @@ public class ClassesActivity extends BaseActivity {
             public ViewHolder(View itemView) {
                 super(itemView);
 
-                mTextView = (TextView) findViewById(R.id.deckName);
+                mTextView = (TextView) itemView.findViewById(R.id.deckName);
             }
-        }
-
-        public DeckAdapter(Context context, List<Deck> data) {
-            mInflater = LayoutInflater.from(context);
-            mDataModel = data;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = mInflater.inflate(R.layout.deck_item, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            Deck model = mDataModel.get(position);
-
-            // TODO: Bind elements from Deck
-            holder.mTextView.setText(model.getTitle());
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDataModel.size();
         }
     }
 }
