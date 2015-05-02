a21eee0459d9d149ea97325165ea9b877729d7ee
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-05-01 21:52:51 -0700

    Created generic class to save to cognito

---
+++ app/src/main/java/edu/csuchico/facematchroster/helper/SaveToCognitoTask.java
@@ -0,0 +1,76 @@
+package edu.csuchico.facematchroster.helper;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+import edu.csuchico.facematchroster.model.ClassModel;
+import edu.csuchico.facematchroster.model.Student;
+
+import static edu.csuchico.facematchroster.util.LogUtils.LOGD;
+import static edu.csuchico.facematchroster.util.LogUtils.makeLogTag;
+
+/**
+ * Created by Pedro Henrique on 5/1/15 - 7:26 PM.
+ */
+public class SaveToCognitoTask extends AsyncTask<Object, Integer, Boolean> {
+
+    private static final String TAG = makeLogTag(SaveToCognitoTask.class);
+
+    private Context mContext;
+
+    public SaveToCognitoTask(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    protected Boolean doInBackground(Object... objects) {
+
+        Object model = getModelToSave(objects);
+
+        try {
+            //TODO: use mapper.save(student, new DynamoDBMapperConfig(new TableNameOverride(tableName)));
+            // to make table name dynamic(get from input/email/consistent as table name stays same in db)
+            //TODO: error checking for inputs
+            //TODO: post success or failure of upload
+
+            //Initialize the Amazon Cognito credentials provider
+            CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                    mContext, // Context
+                    "us-east-1:bd3ecd92-f22f-4dc0-a0b5-bcc79294044b", // Identity Pool ID
+                    Regions.US_EAST_1 // Region
+            );
+            LOGD(TAG, "my ID is " + credentialsProvider.getIdentityId());
+
+            AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+            DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+
+            //TODO: query table to see if user exists already
+            //TODO: if user exists ask if he wants to update
+            mapper.save(model);
+            //this should be used
+            //mapper.save(student, new DynamoDBMapperConfig(new TableNameOverride(tableName)));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private Object getModelToSave(Object[] objects) {
+        Object model = null;
+
+        if (objects[0] instanceof ClassModel) {
+            model = ((ClassModel) objects[0]);
+        } else if (objects[0] instanceof Student) {
+            model = ((Student) objects[0]);
+        }
+
+        return model;
+    }
+
+}
