c74f5f0d897ef8d164f7eb87d9ceb0523a53aa76
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-05-03 16:01:06 -0700

    save instructor on cognito

--- app/src/main/java/edu/csuchico/facematchroster/LoginActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/LoginActivity.java
@@ -1,18 +1,25 @@
 package edu.csuchico.facematchroster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import edu.csuchico.facematchroster.model.Instructor;
 import edu.csuchico.facematchroster.ui.BaseActivity;
+import edu.csuchico.facematchroster.ui.ClassesActivity;
+import edu.csuchico.facematchroster.util.AccountUtils;
+import edu.csuchico.facematchroster.util.SaveToCognitoHelper;
 
 import static edu.csuchico.facematchroster.util.LogUtils.LOGD;
 import static edu.csuchico.facematchroster.util.LogUtils.makeLogTag;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements SaveToCognitoHelper.OnCognitoResult {
     private static final String TAG = makeLogTag(LoginActivity.class);
 
     @InjectView(R.id.dialog_layout)
@@ -22,6 +29,24 @@ public class LoginActivity extends BaseActivity {
     public void onLoginInstructor() {
         LOGD(TAG, "Login Instructor");
         hideDialogInstructorStudentLayout();
+
+        final MaterialDialog materialDialog =
+                new MaterialDialog.Builder(LoginActivity.this)
+                        .title("Signing in...")
+                        .cancelable(false)
+                        .content("Your phone is contacting our servers")
+                        .progress(true, 0).build();
+
+        SaveToCognitoHelper saveToCognitoHelper = SaveToCognitoHelper.
+                saveToCognitoWithDialog(LoginActivity.this, materialDialog, LoginActivity.this);
+
+        Instructor instructor = new Instructor(
+                "1", // id TODO: what's gonna be the id of instructor?
+                AccountUtils.getPlusName(this), // name
+                AccountUtils.getActiveAccountName(this), // email
+                System.currentTimeMillis()); // timestamp
+
+        saveToCognitoHelper.execute(instructor);
     }
 
     @OnClick(R.id.buttonStudent)
@@ -39,5 +64,38 @@ public class LoginActivity extends BaseActivity {
 
     private void hideDialogInstructorStudentLayout() {
         mDialogLayout.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void saveToCognitoResult(boolean result) {
+        if (result == false) {
+            new MaterialDialog.Builder(LoginActivity.this)
+                    .title("Signing in...")
+                    .content("Can't connect")
+                    .positiveText("OK")
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+        } else {
+            new MaterialDialog.Builder(LoginActivity.this)
+                    .title("Hello " + AccountUtils.getPlusName(this))
+                    .content("Welcome to FaceMatch Roster")
+                    .positiveText("Next")
+                    .cancelable(false)
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            dialog.dismiss();
+                            startActivity(new Intent(LoginActivity.this, ClassesActivity.class));
+                            finish();
+
+                        }
+                    })
+                    .show();
+        }
     }
 }
