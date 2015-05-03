9ee8e7bfff211c397403c6bb7a7353410cb81607
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-05-03 15:34:58 -0700

    hide layout when user choose one

--- app/src/main/java/edu/csuchico/facematchroster/LoginActivity.java
+++ app/src/main/java/edu/csuchico/facematchroster/LoginActivity.java
@@ -1,24 +1,43 @@
 package edu.csuchico.facematchroster;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import edu.csuchico.facematchroster.ui.BaseActivity;
 
 import static edu.csuchico.facematchroster.util.LogUtils.LOGD;
 import static edu.csuchico.facematchroster.util.LogUtils.makeLogTag;
 
-
 public class LoginActivity extends BaseActivity {
-
     private static final String TAG = makeLogTag(LoginActivity.class);
+
+    @InjectView(R.id.dialog_layout)
+    LinearLayout mDialogLayout;
+
+    @OnClick(R.id.buttonInstructor)
+    public void onLoginInstructor() {
+        LOGD(TAG, "Login Instructor");
+        hideDialogInstructorStudentLayout();
+    }
+
+    @OnClick(R.id.buttonStudent)
+    public void onLoginStudent() {
+        LOGD(TAG, "Login Student");
+        hideDialogInstructorStudentLayout();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
     }
 
+    private void hideDialogInstructorStudentLayout() {
+        mDialogLayout.setVisibility(View.GONE);
+    }
 }
