dccbe45f54e1363075aed25cff0dddb436df211a
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-05-09 16:36:23 -0700

    remvoed unnecessary methods

--- app/src/main/java/edu/csuchico/facematchroster/util/AccountUtils.java
+++ app/src/main/java/edu/csuchico/facematchroster/util/AccountUtils.java
@@ -17,24 +17,15 @@ package edu.csuchico.facematchroster.util;
  */
 
 import android.accounts.Account;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
-import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.auth.UserRecoverableNotifiedException;
 import com.google.android.gms.common.Scopes;
 
-import java.io.IOException;
-
 import static edu.csuchico.facematchroster.util.LogUtils.LOGD;
-import static edu.csuchico.facematchroster.util.LogUtils.LOGE;
-import static edu.csuchico.facematchroster.util.LogUtils.LOGI;
-import static edu.csuchico.facematchroster.util.LogUtils.LOGV;
-import static edu.csuchico.facematchroster.util.LogUtils.LOGW;
 
 /**
  * Account and login utilities. This class manages a local shared preferences object
@@ -47,6 +38,9 @@ public class AccountUtils {
             Scopes.PLUS_LOGIN,
             "https://www.googleapis.com/auth/userinfo.email"};
     static final String AUTH_TOKEN_TYPE;
+    private static final String PREF_ACTIVE_ACCOUNT = "chosen_account";
+    private static final String PREFIX_PREF_PLUS_PROFILE_ID = "plus_profile_id_";
+    private static final String PREFIX_PREF_PLUS_NAME = "plus_name_";
 
     static {
         StringBuilder sb = new StringBuilder();
@@ -57,12 +51,6 @@ public class AccountUtils {
         }
         AUTH_TOKEN_TYPE = sb.toString();
     }
-
-    private static final String PREF_ACTIVE_ACCOUNT = "chosen_account";
-    // these names are are prefixes; the account is appended to them
-    private static final String PREFIX_PREF_AUTH_TOKEN = "auth_token_";
-    private static final String PREFIX_PREF_PLUS_PROFILE_ID = "plus_profile_id_";
-    private static final String PREFIX_PREF_PLUS_NAME = "plus_name_";
 
     private static SharedPreferences getSharedPreferences(final Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
@@ -102,48 +90,12 @@ public class AccountUtils {
         return prefix + accountName;
     }
 
-    public static String getAuthToken(final Context context) {
-        SharedPreferences sp = getSharedPreferences(context);
-        return hasActiveAccount(context) ?
-                sp.getString(makeAccountSpecificPrefKey(context, PREFIX_PREF_AUTH_TOKEN), null) : null;
-    }
-
-    public static void setAuthToken(final Context context, final String accountName, final String authToken) {
-        LOGI(AmazonAwsUtils.TAG, "Auth token of length "
-                + (TextUtils.isEmpty(authToken) ? 0 : authToken.length()) + " for "
-                + accountName);
-        SharedPreferences sp = getSharedPreferences(context);
-        sp.edit().putString(makeAccountSpecificPrefKey(accountName, PREFIX_PREF_AUTH_TOKEN),
-                authToken).apply();
-        LOGV(AmazonAwsUtils.TAG, "Auth Token: " + authToken);
-    }
-
-    public static void setAuthToken(final Context context, final String authToken) {
-        if (hasActiveAccount(context)) {
-            setAuthToken(context, getActiveAccountName(context), authToken);
-        } else {
-            LOGE(AmazonAwsUtils.TAG, "Can't set auth token because there is no chosen account!");
-        }
-    }
-
-    static void invalidateAuthToken(final Context context) {
-        GoogleAuthUtil.invalidateToken(context, getAuthToken(context));
-        setAuthToken(context, null);
-    }
-
     public static boolean hasPlusInfo(final Context context, final String accountName) {
         SharedPreferences sp = getSharedPreferences(context);
         return !TextUtils.isEmpty(sp.getString(makeAccountSpecificPrefKey(accountName,
                 PREFIX_PREF_PLUS_PROFILE_ID), null));
     }
 
-    public static boolean hasToken(final Context context, final String accountName) {
-        SharedPreferences sp = getSharedPreferences(context);
-        return !TextUtils.isEmpty(sp.getString(makeAccountSpecificPrefKey(accountName,
-                PREFIX_PREF_AUTH_TOKEN), null));
-    }
-
-    @SuppressLint("CommitPrefEdits")
     public static void setPlusName(final Context context, final String accountName, final String name) {
         SharedPreferences sp = getSharedPreferences(context);
         sp.edit().putString(makeAccountSpecificPrefKey(accountName, PREFIX_PREF_PLUS_NAME),
@@ -154,33 +106,5 @@ public class AccountUtils {
         SharedPreferences sp = getSharedPreferences(context);
         return hasActiveAccount(context) ? sp.getString(makeAccountSpecificPrefKey(context,
                 PREFIX_PREF_PLUS_NAME), null) : null;
-    }
-
-    public static void refreshAuthToken(Context mContext) {
-        invalidateAuthToken(mContext);
-//        tryAuthenticateWithErrorNotification(mContext, ScheduleContract.CONTENT_AUTHORITY);
-    }
-
-    static void tryAuthenticateWithErrorNotification(Context context, String syncAuthority) {
-        try {
-            String accountName = getActiveAccountName(context);
-            if (accountName != null) {
-                LOGI(AmazonAwsUtils.TAG, "Requesting new auth token (with notification)");
-                final String token = GoogleAuthUtil.getTokenWithNotification(context, accountName, AUTH_TOKEN_TYPE,
-                        null, syncAuthority, null);
-                setAuthToken(context, token);
-            } else {
-                LOGE(AmazonAwsUtils.TAG, "Can't try authentication because no account is chosen.");
-            }
-
-        } catch (UserRecoverableNotifiedException e) {
-            // Notification has already been pushed.
-            LOGW(AmazonAwsUtils.TAG, "User recoverable exception. Check notification.", e);
-        } catch (GoogleAuthException e) {
-            // This is likely unrecoverable.
-            LOGE(AmazonAwsUtils.TAG, "Unrecoverable authentication exception: " + e.getMessage(), e);
-        } catch (IOException e) {
-            LOGE(AmazonAwsUtils.TAG, "transient error encountered: " + e.getMessage());
-        }
     }
 }
