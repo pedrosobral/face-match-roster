9fe1024efa324670118f8f4d610a79d31b2f755f
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-05-09 17:43:36 -0700

    configuring project to use library ActiveAndroid

---
+++ app/src/main/java/edu/csuchico/facematchroster/FaceMatchApplication.java
@@ -0,0 +1,9 @@
+package edu.csuchico.facematchroster;
+
+import com.activeandroid.app.Application;
+
+/**
+ * Created by Pedro Henrique on 5/9/15 - 5:41 PM.
+ */
+public class FaceMatchApplication extends Application {
+}
